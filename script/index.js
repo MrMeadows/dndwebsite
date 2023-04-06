e3567ddc3735e5c510541270008511c574c42098
MrMeadows <92415382+MrMeadows@users.noreply.github.com>
2023-04-06 06:41:45 -0500

    Update index.js

--- script/index.js
+++ script/index.js
@@ -1,3 +1,7 @@
-$("#nav-placeholder").on("load", function () {
-    $.get("html/nav.html");
-});
+function loadNav() {
+    fetch('nav.html')
+        .then(response => response.text())
+        .then(text => document.getElementById('nav-placeholder').innerHTML = text);
+}
+
+function loadNav();
